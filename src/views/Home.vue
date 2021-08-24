de7448a3f9d18c87cef401d62800f06fff9bac9b
or-abdillh <hans.abdillh05@gmail.com>
2021-08-24 14:51:11 +0800

    Create manifest, index.css, view-style dir

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+   <h3>Home</h3>
+</template>
+
+<script>
+
+   export default {
+      name: 'Home',
+      components: {}
+   }
+
+</script>
+
+<style lang="scss">
+   
+   @import './src/scss/view-styles/_home';
+   
+</style>
